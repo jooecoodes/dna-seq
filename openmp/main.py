a156613aede1dd0e6f53c89d11c9e606536693c9
jooecoodes <jefeljoevillacorta@gmail.com>
2025-09-21 21:28:56 +0800

    DNA generate with Shannon's formula

---
+++ openmp/main.py
@@ -0,0 +1,55 @@
+import math
+import random
+
+def make_dna(length, gc_content, repetitiveness):
+    # Count how many G+C and A+T blocks we need
+    total_GC = round(length * gc_content)
+    total_AT = length - total_GC
+    
+    # Find the perfect mix using our math trick
+    best_skew = find_perfect_mix(gc_content, repetitiveness)
+    
+    # Count exactly how many of each block we need
+    num_G = round(best_skew * total_GC)
+    num_C = total_GC - num_G
+    num_A = round(best_skew * total_AT)
+    num_T = total_AT - num_A
+
+    # Put all blocks in a bag and shake them up!
+    blocks = ['G'] * int(num_G) + ['C'] * int(num_C) + ['A'] * int(num_A) + ['T'] * int(num_T)
+    random.shuffle(blocks)
+    
+    # Build our DNA!
+    return ''.join(blocks)
+
+def find_perfect_mix(gc_content, target_entropy):
+    # This is the math trick to find the perfect mix
+    # We try different combinations until we find the best one
+    best_skew = 0.5
+    best_diff = abs(calculate_entropy(0.5, gc_content) - target_entropy)
+    
+    for test_skew in [i/10000.0 for i in range(0, 10001)]:
+        current_entropy = calculate_entropy(test_skew, gc_content)
+        diff = abs(current_entropy - target_entropy)
+        if diff < best_diff:
+            best_diff = diff
+            best_skew = test_skew
+            
+    return best_skew
+
+def calculate_entropy(skew, gc_content):
+    # This is Shannon's special formula for measuring "mixed-up-ness"
+    fG = skew * gc_content
+    fC = (1 - skew) * gc_content
+    fA = skew * (1 - gc_content)
+    fT = (1 - skew) * (1 - gc_content)
+    
+    entropy = 0.0
+    for freq in [fG, fC, fA, fT]:
+        if freq > 0:
+            entropy -= freq * math.log2(freq)
+    return entropy
+
+# Let's build some DNA!
+my_dna = make_dna(length=100, gc_content=0.4, repetitiveness=1.5)
+print("Here's the DNA I built:", my_dna)
